cf354a1617dbfa18947091a08abecc51b4664aec
noer21 <noer.herlambang@gmail.com>
2017-12-03 20:55:47 -0800

    register login

--- src/pages/register/register.ts
+++ src/pages/register/register.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { LoginPage } from '../login/login';
 
 /**
  * Generated class for the RegisterPage page.
@@ -18,15 +19,48 @@ export class RegisterPage {
   submitted = false;
   status:string;
   lihat = true;
+  status2:string;
+  lihat2 = true;
 
+  nomor:number;
+  name:string;
   email: string;
   password: string;
+  password2:string;
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad RegisterPage');
+  }
+
+  showPassword(){
+    this.status = "text";
+    this.lihat = false;
+    console.log(this.status);
+  }
+
+  hidePassword(){
+    this.status = "password";
+    this.lihat = true;
+    console.log(this.status);
+  }
+
+  showPassword2(){
+    this.status2 = "text";
+    this.lihat2 = false;
+    console.log(this.status);
+  }
+
+  hidePassword2(){
+    this.status2 = "password";
+    this.lihat2 = true;
+    console.log(this.status);
+  }
+
+  signIn() {
+    this.navCtrl.push(LoginPage);
   }
 
 }
