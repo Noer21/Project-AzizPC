3c537b72085a2071b0e153f6ca5f12a07e35108e
noer21 <noer.herlambang@gmail.com>
2018-01-29 00:50:54 -0800

    ganti detail page nya

--- src/pages/detail/detail.ts
+++ src/pages/detail/detail.ts
@@ -15,6 +15,8 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class DetailPage {
 
+  disableButton = true;
+
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
 
